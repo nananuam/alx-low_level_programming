c80e4cbaa43ec9342fb4d4ed3157d6839c2f7d77
nananuam <nuamahe01@gmail.com>
2023-02-22 17:40:45 -0800

    come

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
-void positive_or_negative(int i);
-
-#endif /* MAIN_H */
+#include "main.h"
+/**
+ * main - Test function for positive or negative
+ * Return: 0
+ */
+* main - Test function for positive or negative
+* Return: 0
+*/
+int main(void)
+{
+ int i;
+ i = 0;
+ positive_or_negative(i);
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
